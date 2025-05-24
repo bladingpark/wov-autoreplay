78e1aa4cf53d7335cf1d7f611c559c050255fee8
bladingpark <ac6604878@gmail.com>
2025-05-24 23:11:43 +0200

    log

--- main.js
+++ main.js
@@ -9,18 +9,28 @@ function click(element) {
 
 setInterval(() => {
   const startGame = $('div:contains("START GAME")')
-  if (startGame.length) click(startGame[startGame.length - 1])
+  if (startGame.length) {
+    console.log('Click on "START GAME"')
+    click(startGame[startGame.length - 1])
+  }
 
   const Continue = $('div:contains("Continue")')
-  if (Continue.length) click(Continue[Continue.length - 1])
+  if (Continue.length) {
+    console.log('Click on "Continue"')
+    click(Continue[Continue.length - 1])
+  }
 
   const playAgain = $('div:contains("Play again")')
   if (playAgain.length) {
+    console.log('Click on "Play again"')
     click(playAgain[playAgain.length - 1])
 
     setTimeout(() => {
       const ok = $('div:contains("OK")')
-      if (ok.length) click(ok[ok.length - 1])
+      if (ok.length) {
+        console.log('Click on "OK"')
+        click(ok[ok.length - 1])
+      }
     }, 1000)
   }
 }, 1000)
